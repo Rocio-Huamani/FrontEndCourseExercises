d75a4d0dca6b7fe5eb3111a424d1f823309c6ad3
Rocio Huamani <rociohuamanicuba@gmail.com>
2021-06-20 23:48:25 +0200

    first try with caesar

--- Fundamentals 5/caesar/caesar.js
+++ Fundamentals 5/caesar/caesar.js
@@ -1,5 +1,26 @@
-const caesar = function() {
 
+const caesar = function(str, number) {
+
+if (number<0){
+    return caesar(str,number+26);
+} 
+let output= "";
+
+for (let i=0; i<str.length; i++) {
+    let c= str[i];
+    if (c.match(/[a-z]/i)){
+        let code = str.chartCodeAt(i);
+        if (code >= 65 && code <=90) {
+            c= string.fromCharCode(((code-65+amount)%26)+65);
+        }
+        else if(code >= 97 && code <= 90){
+            c= String.fromCharCode(((code - 97 + amount)%26)+97);
+        }
+        }
+        output += c;
+    }
+    return output;
 }
+
 
 module.exports = caesar
